6aea1f3240cb85e5d09e4689c42a2155cabf56ea
Faheem <faheemuddinsayyed789@gmail.com>
2024-02-09 09:51:29 +0530

    uploaded codes

--- prime_even_odd_switch.c
+++ prime_even_odd_switch.c
@@ -1,42 +1,50 @@
 #include <stdio.h>
 
 int main() {
-    int num;
+    int num,n;
     int isPrime = 1;
-    printf("Enter a number: ");
+    printf("\nEnter accordingly:\n1. Prime or Composite\n2. Even or Odd\n");
+    scanf("%d",&n);
+    printf("\nEnter a number: ");
     scanf("%d", &num);
-    
-    // Checking entered number is prime or composite:
-    switch (num) {
-        case 0:
+
+    switch (n) {
         case 1:
-            printf("%d is neither prime nor composite.\n", num);
+            switch (num) {
+            case 0:
+            case 1:
+                printf("%d is neither prime nor composite.\n", num);
+                break;
+            case 2:
+                printf("%d is a prime number.\n", num);
+                break;
+            default:
+                for (int i = 2; i < num; i++) {
+                    if (num % i == 0) {
+                        isPrime = 0;
+                        break;
+                    }
+                }
+                if (isPrime) {
+                    printf("%d is a prime number.\n", num);
+                } else {
+                    printf("%d is a composite number.\n", num);
+                }
+                break;
+            }
             break;
         case 2:
-            printf("%d is a prime number.\n", num);
-            break;
+            switch (num % 2) {
+            case 0:
+                printf("%d is an even number.\n", num);
+                break;
+            case 1:
+                printf("%d is an odd number.\n", num);
+                break;
+            }    
+            break;  
         default:
-            for (int i = 2; i < num; i++) {
-                if (num % i == 0) {
-                    isPrime = 0;
-                    break;
-                }
-            }
-            if (isPrime) {
-                printf("%d is a prime number.\n", num);
-            } else {
-                printf("%d is a composite number.\n", num);
-            }
-            break;
-    }
-
-    // Checking entered number is even or odd:
-    switch (num % 2) {
-        case 0:
-            printf("%d is an even number.\n", num);
-            break;
-        case 1:
-            printf("%d is an odd number.\n", num);
+            printf("\nPlease enter a valid number.\n");
             break;
     }
 
